9ccf27f55c6207e559eb7702018dfd90d1f0165e
Carlos Urquiza and Scott Scholz <carlos@>
2016-07-06 20:16:45 -0700

    Initial WIP bank project files

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+//business logic
+function Person(name, initialDeposit) {
+  this.fullName = name;
+  this.balance = initialDeposit;
+}
+
+Person.prototype.addDeposit = function(deposit){
+  return this.balance += deposit;
+}
+
+// user interface logic
+
+$(document).ready(function() {
+
+  $("form#new-account").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFullName = ($("input#new-full-name").val());
+    var inputtedInitialDeposit = parseInt($("input#new-initial-deposit").val());
+    console.log(inputtedFullName + " " + inputtedInitialDeposit);
+
+    var newPerson = new Person(inputtedFullName, inputtedInitialDeposit);
+    console.log(newPerson)
+    });
+
+
+});
